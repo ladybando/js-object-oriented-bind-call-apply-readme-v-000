63df1932cbfd2f24f32c9f665fb01643e4c8e474
Ayana Bando <ayanaflax@yahoo.com>
2018-12-17 18:28:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // use this to copy code snippets or use your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+greet(); // my name is , hi!
+ 
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+ 
+person.greet();
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+let sally = { name: 'Sally' };
+ 
+greet.call(sally);
+greet.apply(sally);
+
+function greets(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+
+greets.call(sally, 'Terry', 'George');
+greets.apply(sally, ['Terry', 'George']);
+
+function greetz(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greetz.bind(sally);
+newGreet('Bob');
+greetz('Bob');
