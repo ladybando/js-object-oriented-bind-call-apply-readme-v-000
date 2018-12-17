a2822a666c95de2f2d4793340bf9f0557ecc3bfa
Ayana Bando <ayanaflax@yahoo.com>
2018-12-17 18:33:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -62,3 +62,5 @@ class User {
 let billy = new User('billy', ['music', 'art', 'movies']);
 let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
 
+
+billy.matchInterests(freeMusic);
